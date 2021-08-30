d28c61035e278308419e9eb811b04966e5a83202
Shinu Mathew <shinu.mathew@samsung.com>
2021-08-30 09:47:19 +0530

    First commit

---
+++ helpers/core/common-utils.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+class CommonUtils {
+
+    async hashPassword(password) {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        // await bcrypt.hash(password, 10)  /* generates a salt of 'n' rounds automatically based on the second arg */      
+        return hashedPassword.trim();
+    }
+}
+
+module.exports = CommonUtils;
---
+++ server.js
@@ -0,0 +1,23 @@
+const Express = require('express'),
+    cors = require('cors'),    
+    PostRoutes = require('./lib/routes/posts/post-routes'),
+    AuthRoutes = require('./lib/routes/auth/auth-routes');
+
+const app = Express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(Express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        "status": "SUCCESS"
+    })
+})
+
+app.use('/post',PostRoutes)
+app.use('/auth',AuthRoutes)
+
+
+app.listen(PORT, () => console.log(`Server started at PORT : ${PORT}`))
